79621df964d10d5f3f1c3c11fbcf34f4336aab41
maxim kim <maximk@umich.edu>
2025-05-29 13:00:54 -0400

    minimal example

--- tasks.py
+++ tasks.py
@@ -4,5 +4,9 @@ from robocorp.tasks import task
 
 @task
 def minimal_task():
-    message = "Hello"
-    message = message + " World!"
+    browser = Selenium()
+    browser.open_available_browser("https://www.google.com")
+    browser.input_text("name=q", "hello world")
+    browser.press_keys("name=q", "ENTER")
+    browser.wait_until_element_is_visible("id=search", timeout=10)
+    browser.close_browser()
